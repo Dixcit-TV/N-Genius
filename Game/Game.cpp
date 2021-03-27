6f92642ef9019fed05aeb890afd9c87f07750f4c
Thomas Vincent <thomasvincent0495@gmail.com>
2021-03-27 11:43:53 +0100

    Added Game project and changed Engine to static library

---
+++ Game/Game.cpp
@@ -0,0 +1,14 @@
+#if _DEBUG
+// ReSharper disable once CppUnusedIncludeDirective
+#include <vld.h>
+#endif
+
+#include <Core.h>
+
+int main()
+{
+    ngenius::Core core;
+    core.Run();
+    return 0;
+}
+
