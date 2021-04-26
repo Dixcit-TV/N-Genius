171c531a620cbdac42641868fa65b5c984d7d43e
Thomas Vincent <thomasvincent0495@gmail.com>
2021-04-26 10:44:34 +0200

    removed unused classes (test class)

--- N-Genius/SpriteSheet.cpp
+++
@@ -1,8 +0,0 @@
-#include "SpriteSheet.h"
-
-ngenius::SpriteSheet::SpriteSheet(SDL_Texture* texture, uint32_t frameCount, uint32_t columnCount)
-	: Texture2D(texture)
-	, m_FrameSize(m_TextureSize.x / columnCount, m_TextureSize.y * (columnCount / frameCount))
-	, m_ColumnCount(columnCount)
-	, m_FrameCount(frameCount)
-{}
